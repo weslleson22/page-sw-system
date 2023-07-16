3ec68863d586b0e676acdd7692af1f62cba6b0cc
Wesleson22 <wesllesonsowza@gmail.com>
2023-07-16 20:09:37 -0300

    page em construção

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        🚧 Page MeAlzheimer em Construção 🚧.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://my-portfolio-wesleson.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dev Wesleson
         </a>
       </header>
     </div>
